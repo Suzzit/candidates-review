6ad28a690005801188c515a9403c2d8cf71853bc
suzzit <bajagainsuzzit@gmail.com>
2026-06-13 00:52:19 +0545

    basic script to create required db tables

---
+++ backend/create_tables.py
@@ -0,0 +1,13 @@
+#! ./venv/bin/python
+
+from app.models import engine
+from app.models import Base
+from app.models import users
+from app.models import score
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully!")
+
+if __name__ == "__main__":
+    create_tables()
