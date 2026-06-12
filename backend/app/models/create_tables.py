4bd4f09dc9cd97f3e6246d6c0d3cb04923db1790
suzzit <bajagainsuzzit@gmail.com>
2026-06-13 00:58:47 +0545

    automate creates required database tables

---
+++ backend/app/models/create_tables.py
@@ -0,0 +1,9 @@
+from schema import engine
+from models import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully!")
+
+if __name__ == "__main__":
+    create_tables()
