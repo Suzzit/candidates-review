f707a3d924c97a62d5cb0dafbf327d9629e6c2ef
suzzit <bajagainsuzzit@gmail.com>
2026-06-13 00:53:55 +0545

    added cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,3 +7,11 @@ import app.routers.candidates as candidate_router
 app = FastAPI()
 app.include_router(auth_router.router)
 app.include_router(candidate_router.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=r"http://localhost:\d+",
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
