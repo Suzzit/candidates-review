0910654f4f77acfaf801ba4e2fcd97e0ec9651a7
suzzit <bajagainsuzzit@gmail.com>
2026-06-16 01:28:43 +0545

    user registration and login moved to auth

--- backend/app/routers/auth/__init__.py
+++ backend/app/routers/auth/__init__.py
@@ -1,52 +1,26 @@
 import datetime
 
+from app.make_request import make_post_request, make_register_request
 import sqlalchemy as sa
 from fastapi import HTTPException, APIRouter
 
 from app.models import engine
-from app.routers.auth.utils import hash_password
-from app.routers.auth.jwt_utils import encode_payload
-from app.models.users import Users
 
 router = APIRouter()
 
 @router.post("/api/login")
 async def login(email: str, password: str):
-    hashed_password = hash_password(password)
-
-    query = sa.select(
-        Users.id,
-        Users.name,
-        Users.password,
-        Users.email,
-        Users.role,
-        Users.role_applied,
-        Users.status,
-    ).where(
-        Users.email == email,
-    )
-
-    with engine.connect() as connection:
-        result = connection.execute(query).fetchone()
-
-    if result is None:
-        raise HTTPException(status_code=401, detail="Invalid email")
-
-    if result.password != hashed_password:
-        raise HTTPException(status_code=401, detail="Invalid password")
-
-    payload = {
-        **result._asdict(),
-        "iat": datetime.datetime.utcnow(),
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+    data = {
+        'email': email,
+        'password': password,
     }
 
-    del payload["password"]
+    auth_response =  make_post_request(data)
 
     return {
         "message": "Logged in successfully",
         "status": 200,
-        "auth_token": encode_payload(payload),
+        "auth_token": auth_response.get('token'),
     }
 
 
@@ -57,22 +31,13 @@ async def register(name: str, email: str, password: str, role_applied: str):
             status_code=400, detail="name, email, password, role_applied are required"
         )
 
-    hashed_password = hash_password(password)
-
-    new_user = {
+    payload = {
         "name": name,
         "email": email,
-        "password": hashed_password,
+        "password": password,
         "role_applied": role_applied,
-        "status": "new",
-        "role": "reviewer",
     }
-
-    insert_query = sa.insert(Users).values(**new_user)
-
-    with engine.connect() as connection:
-        connection.execute(insert_query)
-        connection.commit()
+    new_user = make_register_request(payload)
 
     return {
         "success": True,
