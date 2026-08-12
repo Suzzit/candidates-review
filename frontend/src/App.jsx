d6fd56df8af18f3494822d0dd4c870e9b20fda43
suzzit <bajagainsuzzit@gmail.com>
2026-08-12 23:18:24 +0545

    rename ProtectedRoute to RoleProtectedRoute and update routing logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './context/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute'
+import RoleProtectedRoute from './components/ProtectedRoute'
 import Layout from './components/Layout'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -26,9 +26,8 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route element={<ProtectedRoute />}>
+        <Route element={<RoleProtectedRoute  roles={['reviewer', 'admin']} />}>
         <Route element={<Layout />}>
-          <Route element={<ProtectedRoute roles={['reviewer', 'admin']} />}>
             <Route path="/candidates" element={<CandidateList />} />
           </Route>
           <Route path="/candidates/:id" element={<CandidateDetail />} />
